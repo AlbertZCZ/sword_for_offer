94029b7e23850be13f0ef38be0a6234da49132a3
zhang <zhangchaozhen1023@hotmail.com>
2018-03-27 19:38:42 +0800

    进制与运算

---
+++ src/main/java/sword/offer/Interview10.java
@@ -0,0 +1,14 @@
+package sword.offer;
+
+/**
+ * @Author:Zhangchaozhen
+ * @Date: Create in 2018/3/27 17:37
+ * @Description:  输入一个整数，输出该数二进制表示中1的个数。
+ * 例如把9表示成二进制是1001 ，有2位是1. 因此如果输入9，该出2。
+ */
+public class Interview10 {
+
+    public static int numberOfOne(int n) {
+        return 0;
+    }
+}
