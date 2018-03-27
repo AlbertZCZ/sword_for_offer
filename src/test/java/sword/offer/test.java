ddf8709ee2a1e6c60f0fa62cc6e873c0d870a593
zhang <zhangchaozhen1023@hotmail.com>
2018-03-27 19:38:59 +0800

    测试用例

--- src/test/java/sword/offer/test.java
+++ src/test/java/sword/offer/test.java
@@ -6,11 +6,6 @@ package sword.offer;
  * @Description: 测试用例
  */
 public class test {
-    public static void main(String[] args) {
-
-        test06();
-    }
-
     public static void test03() {
         int[][] matrix = {
                 {1, 2, 8, 9},
@@ -50,10 +45,54 @@ public class test {
         Interview05.printReversingly(null);
     }
 
+    public test() {
+        super();
+    }
+
     public static void test06() {
         int[] preorder = {1, 2, 4, 7, 3, 5, 6, 8};
         int[] inorder = {4, 7, 2, 1, 5, 3, 8, 6};
         Interview06.TreeNode root = Interview06.construct(preorder,0,7,inorder,0,7);
         Interview06.afterOrder(root);
+    }
+
+    public static void test08() {
+        // 典型输入，单调升序的数组的一个旋转
+        int[] array1 = {3, 4, 5, 1, 2};
+        System.out.println(Interview08.min(array1));
+        // 有重复数字，并且重复的数字刚好的最小的数字
+        int[] array2 = {3, 4, 5, 1, 1, 2};
+        System.out.println(Interview08.min(array2));
+        // 有重复数字，但重复的数字不是第一个数字和最后一个数字
+        int[] array3 = {3, 4, 5, 1, 2, 2};
+        System.out.println(Interview08.min(array3));
+        // 有重复的数字，并且重复的数字刚好是第一个数字和最后一个数字
+        int[] array4 = {1, 0, 1, 1, 1};
+        System.out.println(Interview08.min(array4));
+        // 单调升序数组，旋转0个元素，也就是单调升序数组本身
+        int[] array5 = {1, 2, 3, 4, 5};
+        System.out.println(Interview08.min(array5));
+        // 数组中只有一个数字
+        int[] array6 = {2};
+        System.out.println(Interview08.min(array6));
+        // 数组中数字都相同
+        int[] array7 = {1, 1, 1, 1, 1, 1, 1};
+        System.out.println(Interview08.min(array7));
+        // 输入NULL
+        System.out.println(Interview08.min(null));
+    }
+
+    public static void test09() {
+        System.out.println(Interview09.fibonacci(0));
+        System.out.println(Interview09.fibonacci(1));
+        System.out.println(Interview09.fibonacci(2));
+        System.out.println(Interview09.fibonacci(3));
+        System.out.println(Interview09.fibonacci(4));
+        System.out.println(Interview09.fibonacci(5));
+    }
+
+    public static void main(String[] args) {
+
+        test09();
     }
 }
