2b189b8a21e5e1883a839cdc3f177ab40943883a
zhang <zhangchaozhen1023@hotmail.com>
2018-03-28 16:50:42 +0800

    修改double的比较方法

--- src/main/java/sword/offer/Interview11.java
+++ src/main/java/sword/offer/Interview11.java
@@ -5,7 +5,8 @@ package sword.offer;
  * @Date: 3/27/18 11:28 PM
  * @Descriptoin: 实现函数double Power(double base, int exponent)，求base的exponent次方。
  * 不得使用库函数，同时不需要考虑大数问题。
- *
+ *注：在计算机内表示小数时（float，double）都有误差。判断两个小数是否相等，只能判断它们之差的绝对值是不是在一个很小的范围内。
+ * 如果之差很小，就可以认为它们相等。
  **/
 public class Interview11 {
     /**
@@ -18,7 +19,7 @@ public class Interview11 {
         // 指数为0就返回1
         if (exponent == 0)
             return 1;
-        if (exponent < 0 && base == 0)
+        if (exponent < 0 && equal(base,0))
             throw new RuntimeException("0的负数次幂没有意义");
 
         // 求指数的绝对值
@@ -57,5 +58,17 @@ public class Interview11 {
             result *= base;
         }
         return result;
+    }
+
+    /**
+     * 判断两个浮点型数字是否相等
+     * @param num1
+     * @param num2
+     * @return
+     */
+    private static boolean equal(double num1,double num2) {
+        if ((num1 - num2 > -0.0000001) && (num1 - num2 < 0.0000001))
+            return true;
+        return false;
     }
 }
