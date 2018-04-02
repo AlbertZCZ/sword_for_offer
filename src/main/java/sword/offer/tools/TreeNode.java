ccf6a917a5fbbd1409960dfcd3377a5053d3233b
zhang <zhangchaozhen1023@hotmail.com>
2018-04-02 18:12:05 +0800

    二叉树根节点

---
+++ src/main/java/sword/offer/tools/TreeNode.java
@@ -0,0 +1,21 @@
+package sword.offer.tools;
+
+/**
+ * @Author: Zhangchaozhen
+ * @Date: Create in 2018/4/2 17:44
+ * @Description: 二叉树根节点
+ */
+public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "value=" + value +
+                ", left=" + left.value +
+                ", right=" + right.value +
+                '}';
+    }
+}
