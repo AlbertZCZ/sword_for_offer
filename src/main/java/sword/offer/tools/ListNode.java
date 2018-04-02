adfd4b6b4925d157d4ebc1d4cfb7f3e8262a31b9
zhang <zhangchaozhen1023@hotmail.com>
2018-04-02 16:00:51 +0800

    链表节点

---
+++ src/main/java/sword/offer/tools/ListNode.java
@@ -0,0 +1,11 @@
+package sword.offer.tools;
+
+/**
+ * @Author:Zhangchaozhen
+ * @Date: Create in 2018/4/2 14:17
+ * @Description: 链表节点
+ */
+public class ListNode {
+    public int value;
+    public ListNode next;
+}
