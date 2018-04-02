873a10d7decd16653ce9f7019d76d97d81c26abc
zhang <zhangchaozhen1023@hotmail.com>
2018-04-02 16:32:10 +0800

    增加打印方法

--- src/main/java/sword/offer/tools/ListNode.java
+++ src/main/java/sword/offer/tools/ListNode.java
@@ -8,4 +8,17 @@ package sword.offer.tools;
 public class ListNode {
     public int value;
     public ListNode next;
+
+    /**
+     * 输出该节点之后的元素
+     *
+     * @param head 当前节点
+     */
+    public void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.value + "->");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
 }
